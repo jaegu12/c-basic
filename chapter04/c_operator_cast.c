d984c45c67b24b09c24d2ed2de1bc2d2c61d7ea1
jaegu12 <112970861+jaegu12@users.noreply.github.com>
2022-10-05 10:34:25 +0900

    Add files via upload

---
+++ chapter04/c_operator_cast.c
@@ -0,0 +1,18 @@
+// casting: 형변환
+//  - int(정수) → double(실수)
+
+#include <stdio.h>
+
+int main(void) {
+    int a =20, b= 3;
+    double res;
+
+    // 6.7 =20.0 / 3.0
+
+    res =(double)a / (double)b;
+    printf("a = %d, b = %d\n", a, b);
+    printf("a/b의 결과 : %.1lf\n", res);
+
+    a = (int)res;
+    printf("(int) %.1lf의 결과: %d\n", res, a);
+}
