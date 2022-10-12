c1e9946632af3a6db4674eebcf7d281224e1c471
jaegu12 <112970861+jaegu12@users.noreply.github.com>
2022-10-12 10:48:17 +0900

    Add files via upload

---
+++ chapter05/c_if.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * if문(제어문) 기본 문법
+ * 
+ * if(조건) {
+ * 실행문
+ * } else if(조건) {
+ *   실행문
+ * } else {
+ *   실행문
+ * }
+ */
+
+int main(void) {
+    int a = 20;
+    int b = 0;
+
+    if(a > 30) {
+        printf("a가 30보다 커요!");
+    }
+}
